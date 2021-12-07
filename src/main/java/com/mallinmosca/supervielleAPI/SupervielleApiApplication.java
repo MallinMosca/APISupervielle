ac76ae3a597c9973e91a72c9a1a943b826df69c6
Mallin <mallin.mosca@riovaradero.com.ar>
2021-12-07 11:05:02 -0300

    actualizacion

--- src/main/java/com/mallinmosca/supervielleAPI/SupervielleApiApplication.java
+++ src/main/java/com/mallinmosca/supervielleAPI/SupervielleApiApplication.java
@@ -2,12 +2,24 @@ package com.mallinmosca.supervielleAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import com.mallinmosca.supervielleAPI.config.SwaggerConfig;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class SupervielleApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupervielleApiApplication.class, args);
 	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+ 
+           registry.addResourceHandler("swagger-ui.html")
+                    .addResourceLocations("classpath:/META-INF/resources/");
+ 
+    }
 
 }
--- src/main/java/com/mallinmosca/supervielleAPI/config/CORSConfig.java
+++ src/main/java/com/mallinmosca/supervielleAPI/config/CORSConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.mallinmosca.supervielleAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
